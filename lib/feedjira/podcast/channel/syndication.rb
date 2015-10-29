45c4e17afca0981fbf421444d0ad26b13186115d
Chris Kalafarski <chris@farski.com>
2015-10-29 01:23:18 -0400

    Remove unused code

--- lib/feedjira/podcast/channel/syndication.rb
+++ lib/feedjira/podcast/channel/syndication.rb
@@ -2,11 +2,7 @@ module Feedjira
   module Podcast
     module Channel
       module Syndication
-        module InstanceMethods
-        end
-
         def self.included(_base)
-          base.include(InstanceMethods)
         end
       end
     end
