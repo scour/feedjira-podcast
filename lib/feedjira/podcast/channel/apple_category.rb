7ecbcb5fff6b5bfc695c25b0cb493e15ed7efa6a
Chris Kalafarski <chris@farski.com>
2015-12-12 17:17:00 -0500

    Add missing iTunes categories

--- lib/feedjira/podcast/channel/apple_category.rb
+++ lib/feedjira/podcast/channel/apple_category.rb
@@ -23,34 +23,65 @@ module Feedjira
           ],
           "Comedy" => [],
           "Education" => [
-
+            "Educational Technology",
+            "Higher Education",
+            "K-12",
+            "Language Courses",
+            "Training"
           ],
           "Games & Hobbies" => [
-
+            "Automotive",
+            "Aviation",
+            "Hobbies",
+            "Other Games",
+            "Video Games"
           ],
           "Government & Organizations" => [
-
+            "Local",
+            "National",
+            "Non-Profit",
+            "Regional"
           ],
           "Health" => [
-
+            "Alternative Health",
+            "Fitness & Nutrition",
+            "Self-Help",
+            "Sexuality"
           ],
           "Kids & Family" => [],
           "Music" => [],
           "News & Politics" => [],
           "Religion & Spirituality" => [
-
+            "Buddhism",
+            "Christianity",
+            "Hinduism",
+            "Islam",
+            "Judaism",
+            "Other",
+            "Spirituality"
           ],
           "Science & Medicine" => [
-
+            "Medicine",
+            "Natural Sciences",
+            "Social Sciences"
           ],
           "Society & Culture" => [
-
+            "History",
+            "Personal Journals",
+            "Philosophy",
+            "Places & Travel"
           ],
           "Sports & Recreation" => [
-
+            "Amateur",
+            "College & High School",
+            "Outdoor",
+            "Professional"
           ],
           "Technology" => [
-
+            "Gadgets",
+            "Tech News",
+            "Podcasting",
+            "Software How-To"
           ],
           "TV & Film" => []
         }
